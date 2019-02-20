c1c183146d98408f2a48d4b86ac9a27386b076c4
SvenOu <948077475@qq.com>
2019-02-20 18:27:10 +0800

    fix bug

---
+++ module_application/src/main/java/com/sv/integrated/view/PickModuleDialog.java
@@ -0,0 +1,62 @@
+package com.sv.integrated.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+
+import com.sv.integrated.R;
+
+/**
+ * Created by sven-ou on 2017/3/2.
+ */
+
+public class PickModuleDialog extends Dialog implements View.OnClickListener {
+    private static final String TAG = PickModuleDialog.class.getSimpleName();
+
+    private Button btnGoTestWigetActivity;
+    private Button btnGoMainViewActivity;
+    private Button btnGoSecurityActivity;
+
+    public interface PickModuleListener{
+        void onButtonsClick(View v);
+    }
+    private PickModuleListener pickModuleListener;
+
+    public PickModuleDialog(Context context) {
+        super(context, R.style.translucentDialog);
+        init();
+    }
+
+    private void init() {
+        setCancelable(false);
+        setCanceledOnTouchOutside(false);
+        setContentView(R.layout.dialog_pick_module);
+
+        btnGoTestWigetActivity = findViewById(R.id.btn_goTestWigetActivity);
+        btnGoMainViewActivity = findViewById(R.id.btn_goMainViewActivity);
+        btnGoSecurityActivity = findViewById(R.id.btn_goSecurityActivity);
+
+        btnGoTestWigetActivity.setOnClickListener(this);
+        btnGoMainViewActivity.setOnClickListener(this);
+        btnGoSecurityActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {}
+
+    @Override
+    public void onClick(View v) {
+        if(null != pickModuleListener){
+            pickModuleListener.onButtonsClick(v);
+        }
+    }
+
+    public PickModuleListener getPickModuleListener() {
+        return pickModuleListener;
+    }
+
+    public void setPickModuleListener(PickModuleListener pickModuleListener) {
+        this.pickModuleListener = pickModuleListener;
+    }
+}
--- module_application/src/main/java/com/sv/integrated/view/ShellActivity.java
+++ module_application/src/main/java/com/sv/integrated/view/ShellActivity.java
@@ -3,25 +3,36 @@ package com.sv.integrated.view;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.sv.integrated.R;
 
-public class ShellActivity extends AppCompatActivity {
-
+public class ShellActivity extends AppCompatActivity implements PickModuleDialog.PickModuleListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_shell);
-
+        PickModuleDialog dialog = new PickModuleDialog(this);
+        dialog.setPickModuleListener(this);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-//                ARouter.getInstance().build("/module_security/view/SecurityActivity").navigation();
-//                ARouter.getInstance().build("/module_guest/main/MainViewActivity").navigation();
-                ARouter.getInstance().build("/lib_common/test/TestWigetActivity").navigation();
-                ShellActivity.this.finish();
+                dialog.show();
             }
         },250);
+    }
+
+    @Override
+    public void onButtonsClick(View v) {
+        int btnId = v.getId();
+        if(btnId == R.id.btn_goTestWigetActivity){
+            ARouter.getInstance().build("/lib_common/test/TestWigetActivity").navigation();
+        }else if(btnId == R.id.btn_goMainViewActivity){
+            ARouter.getInstance().build("/module_guest/main/MainViewActivity").navigation();
+        }else if(btnId == R.id.btn_goSecurityActivity){
+            ARouter.getInstance().build("/module_security/view/SecurityActivity").navigation();
+        }
+        ShellActivity.this.finish();
     }
 }
